0db1d0c920d301df7e27a9a8c324a8043ffdbc5d
2218675712 <2218675712@qq.com>
2020-11-04 22:32:16 +0800

    Dropdown组件点击外部区域自动隐藏

--- src/components/DropDown.vue
+++ src/components/DropDown.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, onMounted, onUnmounted } from 'vue'
 
 export default defineComponent({
   name: 'DropDown',
@@ -23,12 +23,38 @@ export default defineComponent({
   },
   setup () {
     const isOpen = ref(false)
+    // 定义下拉标签
+    const dropdownRef = ref<null|HTMLElement>(null)
+    /**
+     * 切换下拉框
+     */
     const toggleOpen = () => {
       isOpen.value = !isOpen.value
     }
+    /**
+     * 处理点击事件
+     * 点击下面自动关闭下拉框
+     * @param e Element标签
+     */
+    const handler = (e: MouseEvent) => {
+      // 判断是否有值
+      if (dropdownRef.value) {
+        if (!dropdownRef.value.contains(e.target as HTMLElement) && isOpen.value) {
+          isOpen.value = false
+        }
+      }
+    }
+    onMounted(() => {
+      // 监听点击事件调用下拉框处理函数
+      document.addEventListener('click', handler)
+    })
+    onUnmounted(() => {
+      document.removeEventListener('click', handler)
+    })
     return {
       isOpen,
-      toggleOpen
+      toggleOpen,
+      dropdownRef
     }
   }
 })
