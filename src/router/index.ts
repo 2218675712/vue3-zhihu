62e6fa21f125cc4ba2367f87a9ab7d78ce9c2768
2218675712 <2218675712@qq.com>
2020-11-08 20:14:13 +0800

    改进使用router文件

---
+++ src/router/index.ts
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import Home from '@/views/Home.vue'
+import Login from '@/views/Login.vue'
+import ColumnDetail from '@/views/ColumnDetail.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  }, {
+    path: '/login',
+    name: 'login',
+    component: Login
+  }, {
+    path: '/column',
+    name: 'column',
+    component: ColumnDetail
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
