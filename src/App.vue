bcbc70044c5dfebefeb636de7ccbece2de41ea14
2218675712 <2218675712@qq.com>
2020-11-14 12:28:06 +0800

    Loading组件实现

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="container">
-    <global-header :user="currentUser"></global-header>
-    <h1 v-if="isLoading">正在加载</h1>
-    <router-view></router-view>
-    <footer class="text-center py-4 text-secondary bg-light mt-6">
-      <small>
-        <ul class="list-inline mb-0">
-          <li class="list-inline-item">© 2020 知乎专栏</li>
-          <li class="list-inline-item">课程</li>
-          <li class="list-inline-item">文档</li>
-          <li class="list-inline-item">联系</li>
-          <li class="list-inline-item">更多</li>
-        </ul>
-      </small>
-    </footer>
-  </div>
+    <div class="container">
+      <global-header :user="currentUser"></global-header>
+      <loader v-if="isLoading"></loader>
+      <router-view></router-view>
+      <footer class="text-center py-4 text-secondary bg-light mt-6">
+        <small>
+          <ul class="list-inline mb-0">
+            <li class="list-inline-item">© 2020 知乎专栏</li>
+            <li class="list-inline-item">课程</li>
+            <li class="list-inline-item">文档</li>
+            <li class="list-inline-item">联系</li>
+            <li class="list-inline-item">更多</li>
+          </ul>
+        </small>
+      </footer>
+    </div>
 </template>
 
 <script lang="ts">
@@ -22,11 +22,13 @@ import { defineComponent, computed } from 'vue'
 import { useStore } from 'vuex'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import GlobalHeader from '@/components/GlobalHeader.vue'
+import Loader from '@/components/Loader.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    GlobalHeader
+    GlobalHeader,
+    Loader
   },
   setup () {
     const store = useStore()
